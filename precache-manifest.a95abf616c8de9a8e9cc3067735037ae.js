463f4afa603950d313994daab4abe3d434ab5fec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-25 08:34:37 +0000

    Deploy NaCN2020/summercamp to github.com/NaCN2020/summercamp.git:gh-pages

---
+++ precache-manifest.a95abf616c8de9a8e9cc3067735037ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ef70410c9457062be45b97eecc600c",
+    "url": "/summercamp/index.html"
+  },
+  {
+    "revision": "e60d47d0d9c119d2413f",
+    "url": "/summercamp/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "8d836908bb8aace20ad4",
+    "url": "/summercamp/static/css/main.86381c34.chunk.css"
+  },
+  {
+    "revision": "e60d47d0d9c119d2413f",
+    "url": "/summercamp/static/js/2.aa7f4561.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/summercamp/static/js/2.aa7f4561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d836908bb8aace20ad4",
+    "url": "/summercamp/static/js/main.79dabf44.chunk.js"
+  },
+  {
+    "revision": "c1efac7aaa41ad1b7b8b",
+    "url": "/summercamp/static/js/runtime-main.1ec58d64.js"
+  }
+]);
